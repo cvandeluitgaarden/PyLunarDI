8b61f9e817b26a098f099801be7b9f3a504fd34a
Christian van de Luitgaarden <c.vandeluitgaarden@outlook.com>
2023-07-23 00:00:55 +0200

    Art

--- src/LunarDI/LunarDIContainer.py
+++ src/LunarDI/LunarDIContainer.py
@@ -1,19 +1,35 @@
 
+from enum import Enum
 import importlib
 import inspect
 
 from LunarDI.ContainerInterface import ContainerInterface
 
+class DIType(Enum):
+    SINGLETON = 0
+    TRANSIENT = 1
+    
+
 class LunarDIContainer(ContainerInterface):
 
+    IMPL_TYPE = "IT"
+    TYPE = "T"
+
     container = {}
+    impls = {}
 
     def __init__(self):
-        pass
+        self.addSingleton(ContainerInterface, self)
 
     def addTransient(self, type1, type2):
-        self.container.update({ type1: type2 })
+        self.container.update({type1 : { self.IMPL_TYPE: type2, self.TYPE : DIType.TRANSIENT }})
         return self
+
+    def addSingleton(self, type1, type2):
+        self.container.update({type1 : { self.IMPL_TYPE: type2, self.TYPE : DIType.SINGLETON }})
+        self.impls.update({ type1 : type2 })
+        return self
+
 
     def get_args(self, c):
         args = inspect.getfullargspec(c.__init__)
@@ -25,8 +41,19 @@ class LunarDIContainer(ContainerInterface):
 
     def resolve(self, t):
         implType = self.container[t]
-        segments = implType.__module__.split(".")
-        module = importlib.import_module(implType.__module__)
+
+        match implType[self.TYPE]:
+            case DIType.SINGLETON:
+                if not implType[self.IMPL_TYPE] in self.impls:
+                    self.impls.update({ implType[self.IMPL_TYPE] : self.createInst(implType) })
+
+                return self.impls[implType[self.IMPL_TYPE]]
+            case _:
+                return self.createInst(implType)
+    
+    def createInst(self, type1):
+        segments = type1[self.IMPL_TYPE].__module__.split(".")
+        module = importlib.import_module(type1[self.IMPL_TYPE].__module__)
         class_ = getattr(module, segments[-1])
         args = self.get_args(class_)
         
@@ -34,4 +61,3 @@ class LunarDIContainer(ContainerInterface):
             return class_(*args) 
         else: 
             return class_()
-    
--- src/Product/ProductService.py
+++ src/Product/ProductService.py
@@ -1,10 +1,11 @@
+from LunarDI.ContainerInterface import ContainerInterface
 from Product.ProductRepositoryInterface import ProductRepositoryInterface
 from Product.ProductServiceInterface import ProductServiceInterface
 
 
 class ProductService(ProductServiceInterface):
 
-    def __init__(self, repository: ProductRepositoryInterface):
+    def __init__(self, repository: ProductRepositoryInterface, container:ContainerInterface):
         self.repository = repository
 
     def getProduct(self, productNumber:str):
